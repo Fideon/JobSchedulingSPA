e5a7c3281672a64f8944d26b80808c80f68462f4
Fidel Salazar <fidelsf91@gmail.com>
2020-09-09 16:11:37 -0500

    missing semicolons

--- src/app/job-scheduling-page-table/job-scheduling-page-table.component.ts
+++ src/app/job-scheduling-page-table/job-scheduling-page-table.component.ts
@@ -47,11 +47,11 @@ export class JobSchedulingPageTableComponent implements OnInit {
 	}
 
 	formatPhoneNumber(number: number) {
-		var cleaned = ('' + number).replace(/\D/g, '')
-  		var match = cleaned.match(/^(\d{3})(\d{3})(\d{4})$/)
+		var cleaned = ('' + number).replace(/\D/g, '');
+  		var match = cleaned.match(/^(\d{3})(\d{3})(\d{4})$/);
   		if (match) {
-    		return '(' + match[1] + ') ' + match[2] + '-' + match[3]
+    		return '(' + match[1] + ') ' + match[2] + '-' + match[3];
   		}
-  		return null
+  		return null;
 	}
 }
