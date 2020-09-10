8fd533d045cad61049afac94bc8d318e97fa3071
Fidel Salazar <fidelsf91@gmail.com>
2020-09-09 19:13:22 -0500

    Added modules for add component. Added logic and elementsto add component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { AngularFireModule } from '@angular/fire';
-import { AngularFireDatabaseModule } from '@angular/fire/database';
 import { environment } from '../environments/environment';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+
 import { JobSchedulingPageComponent } from './job-scheduling-page/job-scheduling-page.component';
 import { JobSchedulingPageTableComponent } from './job-scheduling-page-table/job-scheduling-page-table.component';
 import { JobSchedulingPageAddnewComponent } from './job-scheduling-page-addnew/job-scheduling-page-addnew.component';
+
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatMomentDateModule } from "@angular/material-moment-adapter";
+
+import { NgxMaskModule, IConfig } from 'ngx-mask'
+
+export let options: Partial<IConfig> | (() => Partial<IConfig>);
 
 @NgModule({
   declarations: [
@@ -17,13 +28,22 @@ import { JobSchedulingPageAddnewComponent } from './job-scheduling-page-addnew/j
     JobSchedulingPageTableComponent,
     JobSchedulingPageAddnewComponent
   ],
+  entryComponents: [
+    JobSchedulingPageAddnewComponent
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebaseConfig),
-    AngularFireDatabaseModule
+    AngularFireDatabaseModule,
+    BrowserAnimationsModule,
+    MatDialogModule,
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatMomentDateModule,
+    NgxMaskModule.forRoot(),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
--- src/app/job-scheduling-page/job-scheduling-page.component.ts
+++ src/app/job-scheduling-page/job-scheduling-page.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { JobSchedulingPageAddnewComponent } from '../job-scheduling-page-addnew/job-scheduling-page-addnew.component';
+
 
 @Component({
-  selector: 'app-job-scheduling-page',
-  templateUrl: './job-scheduling-page.component.html',
-  styleUrls: ['./job-scheduling-page.component.scss']
+	selector: 'app-job-scheduling-page',
+	templateUrl: './job-scheduling-page.component.html',
+	styleUrls: ['./job-scheduling-page.component.scss']
 })
 export class JobSchedulingPageComponent implements OnInit {
 
-  constructor() { }
+	constructor(public dialog: MatDialog) { }
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+	}
 
+	openAddNewDialog() {
+		const addNewDialog = this.dialog.open(JobSchedulingPageAddnewComponent, {
+			width: '300px',
+			height: '750px'
+		});
+	}
 }
